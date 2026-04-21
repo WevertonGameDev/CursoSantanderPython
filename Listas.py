6e49c5ad9f83e60befb40b1e8e33e7244a063584
WevertonGameDev <weverton.gamedev@gmail.com>
2026-04-21 00:14:10 -0300

    Lista com Estrutura de Repetição e Condicional

---
+++ Listas.py
@@ -0,0 +1,4 @@
+numeros = [1, 2, 3, 4]
+quadradosPar = [numero ** 2 for numero in numeros if numero % 2 == 0]
+# Armazena o numero ao quadrado, para numero na lista numeros, armazena numero se for PAR
+print(quadradosPar)
